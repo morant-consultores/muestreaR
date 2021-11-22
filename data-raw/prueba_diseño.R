a3de330465492f942de663a1784cb496f56e3a72
EAMI91 <emiliomorones@gmail.com>
2021-11-22 10:22:19 -0600

    en un mismo script

--- data-raw/prueba_diseño.R
+++ data-raw/prueba_diseño.R
@@ -1,6 +1,22 @@
 library(tidyverse)
 library(glue)
+library(sf)
+library(sp)
 devtools::load_all()
+
+# Crear marco muestral ----------------------------------------------------
+
+wd <- "~/Dropbox (Selva)/Ciencia de datos/Consultoría Estadística/Recursos/Externos/INEGI/Censo 2020"
+
+wd_murb <- list.files(glue("{wd}/Población"), full.names = T)
+wd_loc <- list.files(paste(list.files(glue("{wd}/Localidad"),full.names = T), "conjunto_de_datos",sep = "/"), full.names = T)
+wd_shp <- list.files(paste(list.files(glue("{wd}/AGEB"), full.names = T)[1:32],"conjunto_de_datos", sep = "/"), full.names = T,pattern = "ar.shp")
+orden <- substr(wd_loc,nchar(wd_loc)-13,nchar(wd_loc)-12) %>% order
+
+
+ags <- crear_mm(wd_murb[1],wd_loc[orden][1],wd_shp[1])
+
+# Información muestral ----------------------------------------------------
 
 ja <- empaquetar(ags,
                  c("NOM_MUN","NOM_LOC","AGEB"),
