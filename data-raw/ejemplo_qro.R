46dcf2c72824ee11f742d64edae0991679a03845
EAMI91 <emiliomorones@gmail.com>
2021-11-25 15:08:29 -0600

    con paquetes

--- data-raw/ejemplo_qro.R
+++ data-raw/ejemplo_qro.R
@@ -1,25 +1,25 @@
-library(tidyverse)
-library(glue)
-library(sf)
-library(rgdal)
-library(sp)
-library(rgdal)
+# library(tidyverse)
+# library(glue)
+# library(sf)
+# library(rgdal)
+# library(sp)
+# library(rgdal)
 devtools::load_all()
 
 # Crear marco muestral ----------------------------------------------------
 
 wd <- "~/Dropbox (Selva)/Ciencia de datos/Consultoría Estadística/Recursos/Externos/INEGI/Censo 2020"
 
-wd_murb <- list.files(glue("{wd}/Población"), full.names = T)
-wd_loc <- list.files(paste(list.files(glue("{wd}/Localidad"),full.names = T), "conjunto_de_datos",sep = "/"), full.names = T)
-wd_shp <- list.files(paste(list.files(glue("{wd}/AGEB"), full.names = T)[1:32],"conjunto_de_datos", sep = "/"), full.names = T,pattern = "ar.shp")
-wd_shp_loc <- list.files(paste(list.files(glue("{wd}/AGEB"), full.names = T)[1:32],"conjunto_de_datos", sep = "/"), full.names = T,pattern = "[[:digit:]]l.shp")
+wd_murb <- list.files(glue::glue("{wd}/Población"), full.names = T)
+wd_loc <- list.files(paste(list.files(glue::glue("{wd}/Localidad"),full.names = T), "conjunto_de_datos",sep = "/"), full.names = T)
+wd_shp <- list.files(paste(list.files(glue::glue("{wd}/AGEB"), full.names = T)[1:32],"conjunto_de_datos", sep = "/"), full.names = T,pattern = "ar.shp")
+wd_shp_loc <- list.files(paste(list.files(glue::glue("{wd}/AGEB"), full.names = T)[1:32],"conjunto_de_datos", sep = "/"), full.names = T,pattern = "[[:digit:]]l.shp")
 orden <- substr(wd_loc,nchar(wd_loc)-13,nchar(wd_loc)-12) %>% order
 
-mza <- read_csv(wd_murb[[22]], na = "*")
-loc <- read_csv(wd_loc[orden][22], na = "*")
-ageb_shp <- st_read(wd_shp[22]) %>% st_transform(4326)
-loc_shp <- readOGR(dsn=wd_shp_loc[22],encoding = "CP1252") %>% spTransform(CRS("+init=epsg:4326")) %>% st_as_sf()
+mza <- readr::read_csv(wd_murb[[22]], na = "*")
+loc <- readr::read_csv(wd_loc[orden][22], na = "*")
+ageb_shp <- sf::st_read(wd_shp[22]) %>% sf::st_transform(4326)
+loc_shp <- rgdal::readOGR(dsn=wd_shp_loc[22],encoding = "CP1252") %>% sp::spTransform(sp::CRS("+init=epsg:4326")) %>% sf::st_as_sf()
 
 qro <- crear_mm(mza = mza, loc = loc, ageb_shp = ageb_shp, loc_shp = loc_shp)
 
