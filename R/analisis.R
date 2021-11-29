7622dcabe78ba54c4fe499f814c29c38ec594985
EAMI91 <emiliomorones@gmail.com>
2021-11-29 10:01:25 -0600

    agregar gráficas de análisis (prueba)

--- R/analisis.R
+++ R/analisis.R
@@ -2,6 +2,16 @@ analisis_global_nivel <- function(marco){
   p_18ymas <- marco %>%
     summarise(resultado=sum(P_18YMAS, na.rm=T)) %>% arrange(desc(resultado))
 
+
+
+  gg.p_18ymas <- p_18ymas %>% ggplot(aes(
+    x = ifelse(
+      length(group_vars(marco)) > 0,
+      !!sym(group_vars(marco)),
+      "1"
+    ),
+    y = resultado)) + geom_col()
+
   # Porcentaje de población total:
 
 
@@ -21,8 +31,11 @@ analisis_global_nivel <- function(marco){
     summarise(pobtot=sum(POBTOT), na.rm=T) %>%
     mutate(pct = scales::percent(pobtot/sum(pobtot)))
 
+  gg.p_rural_urbana <- p_rural_urbana %>% ggplot(aes(x = ))
+
   return(
     list(`Población de 18 años y más` = p_18ymas,
+         gg.p_18ymas,
          `Proporción de 18 años y más` = pct_18ymas,
          `Unidades de muestreo` = unidades,
          `Población rural y urbana` = p_rural_urbana)
