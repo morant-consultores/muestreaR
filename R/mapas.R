753538be0d7c2d97db111e92bb7154b2fd07103a
EAMI91 <emiliomorones@gmail.com>
2021-12-02 14:25:22 -0600

    paquete leaflet

--- R/mapas.R
+++ R/mapas.R
@@ -8,13 +8,13 @@ graficar_mapa_poblacion <- function(bd, shp, nivel, variable){
 
   aux <- aux %>% mutate(color = pal(n))
 
-  leaflet() %>%
-    addProviderTiles("CartoDB.DarkMatter") %>%
-    addPolygons(data = aux %>% filter(st_geometry_type(.) != "POINT"),
-                fillColor = ~color, fillOpacity = .7, weight = 1,stroke = T, color = "black") %>%
-    addCircleMarkers(data = aux %>% filter(st_geometry_type(.) == "POINT"),
-                     radius = 1, color = ~color, fillOpacity = 1,stroke = T,opacity = 1, , weight = 1) %>%
-    addLegend(data = aux, pal = pal, values = ~n, title = variable)
+  leaflet::leaflet() %>%
+    leaflet::addProviderTiles("CartoDB.DarkMatter") %>%
+    leaflet::addPolygons(data = aux %>% filter(sf::st_geometry_type(.) != "POINT"),
+                         fillColor = ~color, fillOpacity = .7, weight = 1,stroke = T, color = "black") %>%
+    leaflet::addCircleMarkers(data = aux %>% filter(sf::st_geometry_type(.) == "POINT"),
+                              radius = 1, color = ~color, fillOpacity = 1,stroke = T,opacity = 1, , weight = 1) %>%
+    leaflet::addLegend(data = aux, pal = pal, values = ~n, title = variable)
 
 
 }
